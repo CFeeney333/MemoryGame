5cbb8279cdcb1fbc8aa20756189348b4036dc2c4
ChippySpud <c.feeney333@gmail.com>
2024-03-20 14:47:45 +0000

    Add functions to hide and reveal elements

--- js/main.js
+++ js/main.js
@@ -5,3 +5,15 @@ for (let s of symbols) {
         e.target.classList.toggle('hidden');
     });
 }
+
+const hide = (e) => {
+    if (!e.classList.contains('hidden')) {
+        e.classList.add('hidden');
+    }
+}
+
+const reveal = (e) => {
+    if (e.classList.contains('hidden')) {
+        e.classList.remove('hidden');
+    }
+}
