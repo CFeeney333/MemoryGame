28135e6317bb8f8ee3760c0e863c921fdda5326c
CFeeney333 <c.feeney333@gmail.com>
2024-03-21 16:13:53 +0000

    Add explanatory comments to if statements in event handler

--- js/main.js
+++ js/main.js
@@ -28,10 +28,17 @@ const createBoard = () => {
             hide(symbolElem);
 
             symbolElem.addEventListener('click', (e) => {
+                if (e.target.dataset.matched === "1") {
+                    // it has already been matched
+                    return;
+                }
+
                 if (first === null) {
+                    // if the first one is not already turned over
                     first = e.target;
                     show(first);
                 } else if (second === null && e.target !== first) {
+                    // if the first one is turned over, the second one isn't, and it's not the same element
                     turns.textContent = String(parseInt(turns.textContent) + 1);
 
                     second = e.target;
@@ -39,14 +46,18 @@ const createBoard = () => {
 
                     if (first.dataset.symbol !== second.dataset.symbol) {
                         // retain the elements
+                        // if they are not a match
+                        // retain a reference to the elements, so they can be hidden after the timeout
                         const pair = [first, second];
                         setTimeout(() => {
                             hide(pair[0]);
                             hide(pair[1]);
                         }, FADE_TIMEOUT);
                     } else {
+                        // they are a match
                         pairs.textContent = String(parseInt(pairs.textContent) + 1);
                     }
+                    // reset the first and the second
                     setTimeout(() => {
                         first = null;
                         second = null;
